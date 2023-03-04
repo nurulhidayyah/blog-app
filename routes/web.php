b8d52f7565345009cdebd09ba840d45b16f921e4
nurulhidayyah <nurulhidayyah45@gmail.com>
2023-03-04 21:52:38 +0700

    3. Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Nurul Hidayah',
+        'email' => 'nurulhidayyah45@gmail.com',
+        'image' => 'Nurul_Hidayah.png'
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
